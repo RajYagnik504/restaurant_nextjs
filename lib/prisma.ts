21cf065c5d43e24799be7521822859e8453ad661
Raj Yagnik <rajyagnik@example.com>
2026-08-04 17:10:39 +0530

    Hardcode DATABASE_URL to fix Cloudflare connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { Pool } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
-const connectionString = process.env.DATABASE_URL || '';
+const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_FE6kSc0gNLqT@ep-dry-tree-azxde4bc-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 
 // In Cloudflare Edge, if connectionString is empty, the Neon driver might fallback
 // to attempting to connect to localhost, which will hang the worker and cause a 522 Error.
