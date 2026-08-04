24931e90893b68759515c11980b0fe0b9f15b5c9
Raj Yagnik <rajyagnik@example.com>
2026-08-04 17:25:43 +0530

    Hardcode DATABASE_URL in next.config.ts for Edge runtime

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    DATABASE_URL: 'postgresql://neondb_owner:npg_FE6kSc0gNLqT@ep-dry-tree-azxde4bc-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
